f5f193c522ce42d5e1bf46b8a1cd44c32faea64c
yndi24 <yandicimahi123@gmail.com>
2022-03-22 00:23:23 +0700

    Fix

---
+++ lelang_online.sql
@@ -0,0 +1,250 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 23 Feb 2022 pada 16.55
+-- Versi server: 10.4.22-MariaDB
+-- Versi PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lelang_online`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_barang`
+--
+
+CREATE TABLE `tb_barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(25) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `tgl` date NOT NULL,
+  `harga_awal` int(20) NOT NULL,
+  `deskripsi_barang` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_history_lelang`
+--
+
+CREATE TABLE `tb_history_lelang` (
+  `id_history` int(11) NOT NULL,
+  `id_lelang` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `penawaran_harga` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_lelang`
+--
+
+CREATE TABLE `tb_lelang` (
+  `id_lelang` int(11) NOT NULL,
+  `id_barang` int(11) NOT NULL,
+  `tgl_lelang` date NOT NULL,
+  `harga_akhir` int(20) DEFAULT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_petugas` int(11) NOT NULL,
+  `status` enum('dibuka','ditutup') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_level`
+--
+
+CREATE TABLE `tb_level` (
+  `id_level` int(11) NOT NULL,
+  `level` enum('administrator','petugas') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_level`
+--
+
+INSERT INTO `tb_level` (`id_level`, `level`) VALUES
+(1, 'administrator'),
+(2, 'petugas');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_masyarakat`
+--
+
+CREATE TABLE `tb_masyarakat` (
+  `id_user` int(11) NOT NULL,
+  `nama_lengkap` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `telp` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_masyarakat`
+--
+
+INSERT INTO `tb_masyarakat` (`id_user`, `nama_lengkap`, `username`, `password`, `telp`) VALUES
+(1, 'Fikri Khairul Shaleh', 'fikri', 'aa65989e6e147809f34f559958ecf804', '081281271212'),
+(3, 'Cahyandi', 'user', '5cc32e366c87c4cb49e4309b75f57d64', '08976879434');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_petugas`
+--
+
+CREATE TABLE `tb_petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `nama_petugas` varchar(25) NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `id_level` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `tb_petugas`
+--
+
+INSERT INTO `tb_petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `id_level`) VALUES
+(8, 'Cahyandi', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
+(9, 'Cahyandi', 'petugas', 'afb91ef692fd08c445e8cb1bab2ccf9c', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- Indeks untuk tabel `tb_history_lelang`
+--
+ALTER TABLE `tb_history_lelang`
+  ADD PRIMARY KEY (`id_history`),
+  ADD KEY `id_lelang` (`id_lelang`,`id_barang`,`id_user`),
+  ADD KEY `id_barang` (`id_barang`),
+  ADD KEY `id_user` (`id_user`);
+
+--
+-- Indeks untuk tabel `tb_lelang`
+--
+ALTER TABLE `tb_lelang`
+  ADD PRIMARY KEY (`id_lelang`),
+  ADD KEY `id_barang` (`id_barang`),
+  ADD KEY `id_user` (`id_user`,`id_petugas`),
+  ADD KEY `id_petugas` (`id_petugas`);
+
+--
+-- Indeks untuk tabel `tb_level`
+--
+ALTER TABLE `tb_level`
+  ADD PRIMARY KEY (`id_level`);
+
+--
+-- Indeks untuk tabel `tb_masyarakat`
+--
+ALTER TABLE `tb_masyarakat`
+  ADD PRIMARY KEY (`id_user`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indeks untuk tabel `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  ADD PRIMARY KEY (`id_petugas`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `id_level` (`id_level`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_barang`
+--
+ALTER TABLE `tb_barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_history_lelang`
+--
+ALTER TABLE `tb_history_lelang`
+  MODIFY `id_history` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_lelang`
+--
+ALTER TABLE `tb_lelang`
+  MODIFY `id_lelang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_level`
+--
+ALTER TABLE `tb_level`
+  MODIFY `id_level` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_masyarakat`
+--
+ALTER TABLE `tb_masyarakat`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `tb_history_lelang`
+--
+ALTER TABLE `tb_history_lelang`
+  ADD CONSTRAINT `tb_history_lelang_ibfk_1` FOREIGN KEY (`id_lelang`) REFERENCES `tb_lelang` (`id_lelang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_history_lelang_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_history_lelang_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `tb_masyarakat` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_lelang`
+--
+ALTER TABLE `tb_lelang`
+  ADD CONSTRAINT `tb_lelang_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `tb_barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_lelang_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `tb_masyarakat` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tb_lelang_ibfk_3` FOREIGN KEY (`id_petugas`) REFERENCES `tb_petugas` (`id_petugas`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Ketidakleluasaan untuk tabel `tb_petugas`
+--
+ALTER TABLE `tb_petugas`
+  ADD CONSTRAINT `tb_petugas_ibfk_1` FOREIGN KEY (`id_level`) REFERENCES `tb_level` (`id_level`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
